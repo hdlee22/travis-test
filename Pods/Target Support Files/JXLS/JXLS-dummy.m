4ee8812d06d8b432cc9fa36a554624b9231acbf8
HD <hd.lee@dramancompany.com>
2016-03-16 11:50:10 +0900

    second commit

---
+++ Pods/Target Support Files/JXLS/JXLS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JXLS : NSObject
+@end
+@implementation PodsDummy_JXLS
+@end
---
+++ travis-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travis-test
+//
+//  Created by HD on 2016. 3. 16..
+//  Copyright © 2016년 Drama&Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
