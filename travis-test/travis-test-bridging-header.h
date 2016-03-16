6cfb6afac50e117718852a0cbcffcfd10f64d0e3
HD <hd.lee@dramancompany.com>
2016-03-16 12:20:49 +0900

    6th commit

---
+++ travis-test/travis-test-bridging-header.h
@@ -0,0 +1,24 @@
+//
+//  travis-test-bridging-header.h
+//  travis-test
+//
+//  Created by HD on 2016. 3. 16..
+//  Copyright © 2016년 Drama&Company. All rights reserved.
+//
+
+/// cocoapods
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/AFNetworkActivityIndicatorManager.h>
+#import <DACircularProgress/DACircularProgressView.h>
+#import <Google/Analytics.h>
+#import <iRate/iRate.h>
+#import <JVFloatLabeledTextField/JVFloatLabeledTextField.h>
+#import <JVFloatLabeledTextField/JVFloatLabeledTextView.h>
+#import <libPhoneNumber-iOS/NBPhoneNumberUtil.h>
+#import <libPhoneNumber-iOS/NBPhoneNumber.h>
+#import <libPhoneNumber-iOS/NBAsYouTypeFormatter.h>
+#define MR_SHORTHAND
+#import <CoreData+MagicalRecord.h>
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <SDWebImage/UIButton+WebCache.h>
+#import <TTTAttributedLabel/TTTAttributedLabel.h>
